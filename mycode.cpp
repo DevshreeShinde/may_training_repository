016683c429ca0265d009405fa0ae47a5542d81d6
DevshreeShinde <deveshreeshinde@gmail.com>
2023-05-04 15:59:47 +0530

    Initial comment

--- mycode.cpp
+++ mycode.cpp
@@ -3,6 +3,12 @@ using namespace std;
 int main(){
 cout<<"This is C file";
 	return 0;
+<<<<<<< Updated upstream
 	
 }
 // This is my remote comment
+=======
+}
+
+//This is my local comment.
+>>>>>>> Stashed changes
